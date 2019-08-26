0ad72828acf7ff306c605a0bf54b1f63e91f7b0c
prafesar <blagoveschenskij@gmail.com>
2019-08-26 10:18:20 +0000

    refact parsers.js remove normalize data func

--- src/parsers.js
+++ src/parsers.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
-import _ from 'lodash';
 
 const parserActions = {
   '.json': JSON.parse,
@@ -10,39 +9,9 @@ const parserActions = {
   '.yaml': yaml.load,
 };
 
-const normalizeDate = (content) => {
-  const normalizeValueActions = [
-    { // from string "234" to number 234
-      check: v => (typeof Number(v) === 'number') && (String(Number(v)) === String(v)),
-      action: v => Number(v),
-    },
-    { // from string "true" to boolean true
-      check: v => (typeof Boolean(v) === 'boolean') && (String(Boolean(v)) === String(v)),
-      action: v => Boolean(v),
-    },
-    { // if value is object
-      check: v => _.isObject(v),
-      action: v => normalizeDate(v),
-    },
-    { // if value is string
-      check: v => v === String(v),
-      action: v => v,
-    },
-  ];
-
-  const normalizeValue = (value) => {
-    const { action } = normalizeValueActions.find(({ check }) => check(value));
-    return action(value);
-  };
-
-  return Object.entries(content)
-    .reduce((acc, [key, value]) => ({ ...acc, [key]: normalizeValue(value) }), {});
-};
-
 // get date from file
 export default (filePath) => {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const parse = parserActions[path.extname(filePath)];
-  const parsedContent = parse(fileContent);
-  return normalizeDate(parsedContent);
+  return parse(fileContent);
 };
