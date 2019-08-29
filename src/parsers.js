025f05a0562a59a64620fccb10ba2b2245bbf6c8
prafesar <blagoveschenskij@gmail.com>
2019-08-29 08:45:36 +0000

    refact parser.js split main function

--- src/parsers.js
+++ src/parsers.js
@@ -3,15 +3,18 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parserActions = {
+const parseActions = {
   '.json': JSON.parse,
   '.ini': ini.parse,
   '.yaml': yaml.load,
 };
 
-// get date from file
-export default (filePath) => {
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-  const parse = parserActions[path.extname(filePath)];
-  return parse(fileContent);
+const getParseAction = filePath => parseActions[path.extname(filePath)];
+
+const parseDataFromFile = (filePath) => {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const parse = getParseAction(filePath);
+  return parse(content);
 };
+
+export default parseDataFromFile;
