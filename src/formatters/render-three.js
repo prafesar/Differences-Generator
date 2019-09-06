d66cb67e3befc14f1b563557df345d43166d9e42
prafesar <blagoveschenskij@gmail.com>
2019-09-06 08:59:58 +0000

    refact render-tree.js remove global state and refact main function

--- src/formatters/render-three.js
+++ src/formatters/render-three.js
@@ -2,16 +2,6 @@ import _ from 'lodash';
 
 const tabSize = 4;
 const indent = level => ' '.repeat(level * tabSize);
-let nodeRenderAction = [];
-
-function renderDiffToThree(ast, level = 0) {
-  const result = ast.reduce((acc, node) => {
-    const { action } = nodeRenderAction.find(({ type }) => node.type === type);
-    return [...acc, action(node, level)];
-  }, []);
-
-  return `{\n${result.join('\n')}\n${indent(level)}}`;
-}
 
 const stringify = (inValue, level) => {
   if (!_.isObject(inValue)) {
@@ -24,39 +14,28 @@ const stringify = (inValue, level) => {
   return `{\n${result.join('\n')}\n${indentBrackets}}`;
 };
 
-function renderLeafNode(levl, mark, key, value) {
-  return `${indent(levl)}  ${mark} ${key}: ${stringify(value, levl + 1)}`;
-}
+const renderDiffToThree = (ast, level = 0) => {
+  const renderLeafNode = (levl, mark, key, value) => `${indent(levl)}  ${mark} ${key}: ${stringify(value, levl + 1)}`;
+  const renderThreeNode = (levl, key, children) => `${indent(levl)}    ${key}: ${renderDiffToThree(children, levl + 1)}`;
 
-function renderThreeNode(levl, key, children) {
-  return `${indent(levl)}    ${key}: ${renderDiffToThree(children, levl + 1)}`;
-}
-
-nodeRenderAction = [
-  {
-    type: 'removed',
-    action: ({ key, valueBefore }, nodeLevel) => renderLeafNode(nodeLevel, '-', key, valueBefore),
-  },
-  {
-    type: 'added',
-    action: ({ key, valueAfter }, nodeLevel) => renderLeafNode(nodeLevel, '+', key, valueAfter),
-  },
-  {
-    type: 'nested',
-    action: ({ key, children }, nodeLevel) => renderThreeNode(nodeLevel, key, children),
-  },
-  {
-    type: 'updated',
-    action: ({ key, valueBefore, valueAfter }, nodeLevel) => {
+  const renderAction = {
+    removed: ({ key, valueBefore }, nodeLevel) => renderLeafNode(nodeLevel, '-', key, valueBefore),
+    added: ({ key, valueAfter }, nodeLevel) => renderLeafNode(nodeLevel, '+', key, valueAfter),
+    nested: ({ key, children }, nodeLevel) => renderThreeNode(nodeLevel, key, children),
+    updated: ({ key, valueBefore, valueAfter }, nodeLevel) => {
       const before = renderLeafNode(nodeLevel, '-', key, valueBefore);
       const after = renderLeafNode(nodeLevel, '+', key, valueAfter);
       return `${before}\n${after}`;
     },
-  },
-  {
-    type: 'unchanged',
-    action: ({ key, valueAfter }, nodeLevel) => renderLeafNode(nodeLevel, ' ', key, valueAfter),
-  },
-];
+    unchanged: ({ key, valueAfter }, nodeLevel) => renderLeafNode(nodeLevel, ' ', key, valueAfter),
+  };
+
+  const result = ast.reduce((acc, node) => {
+    const resultNode = renderAction[node.type](node, level);
+    return [...acc, resultNode];
+  }, []);
+
+  return `{\n${result.join('\n')}\n${indent(level)}}`;
+};
 
 export default renderDiffToThree;
