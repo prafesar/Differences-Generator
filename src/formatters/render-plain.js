ba389ec850a768430ed4a1560ea0c7d003b9872c
prafesar <blagoveschenskij@gmail.com>
2019-09-06 08:33:43 +0000

    refact render-plain.js based on node type

--- src/formatters/render-plain.js
+++ src/formatters/render-plain.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 const stringify = (value) => {
   const valueTypes = {
     number: v => v,
@@ -12,24 +10,24 @@ const stringify = (value) => {
 
 const pathToStr = path => path.join('.');
 
-const renderNodeAction = {
-  removed: path => `Property '${pathToStr(path)}' was removed`,
-  added: (path, { valueAfter }) => `Property '${pathToStr(path)}' was added whith value: ${stringify(valueAfter)}`,
-  updated: (path, { valueBefore, valueAfter }) => `Property '${pathToStr(path)}' was updated. From ${stringify(valueBefore)} to ${stringify(valueAfter)}`,
-};
-
 const renderDiffToPlain = (ast, pathAcc = []) => {
+  const renderNodeAction = {
+    removed: path => `Property '${pathToStr(path)}' was removed`,
+    added: (path, { valueAfter }) => `Property '${pathToStr(path)}' was added whith value: ${stringify(valueAfter)}`,
+    updated: (path, { valueBefore, valueAfter }) => `Property '${pathToStr(path)}' was updated. From ${stringify(valueBefore)} to ${stringify(valueAfter)}`,
+    nested: (path, { children }) => renderDiffToPlain(children, path),
+    unchanged: () => '',
+  };
+
   const result = ast
-    .filter(({ type }) => type !== 'unchanged')
     .reduce((acc, node) => {
-      const { key, type, children } = node;
+      const { key, type } = node;
       const path = [...pathAcc, key];
-      if (type === 'nested') {
-        return _.flatten([...acc, renderDiffToPlain(children, path)]);
-      }
       const render = renderNodeAction[type];
       return [...acc, render(path, node)];
-    }, []);
+    }, [])
+    .filter(elem => elem !== '');
+
   return result.join('\n');
 };
 
