c86572dd3bd49bd97edef4bd98480c1e2a064945
prafesar <blagoveschenskij@gmail.com>
2019-09-13 07:37:16 +0000

    style formatters/render-plain.js move container out

--- src/formatters/render-plain.js
+++ src/formatters/render-plain.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 const stringify = (value) => {
   const valueTypes = {
-    number: v => v,
-    boolean: v => v,
+    number: _.identity,
+    boolean: _.identity,
     string: v => `'${v}'`,
     object: () => '[complex value]',
   };
@@ -10,24 +12,24 @@ const stringify = (value) => {
 
 const pathToStr = path => path.join('.');
 
-const renderDiffToPlain = (ast, pathAcc = []) => {
-  const renderNodeAction = {
-    removed: path => `Property '${pathToStr(path)}' was removed`,
-    added: (path, { valueAfter }) => `Property '${pathToStr(path)}' was added whith value: ${stringify(valueAfter)}`,
-    updated: (path, { valueBefore, valueAfter }) => `Property '${pathToStr(path)}' was updated. From ${stringify(valueBefore)} to ${stringify(valueAfter)}`,
-    nested: (path, { children }) => renderDiffToPlain(children, path),
-    unchanged: () => null,
-  };
+const renderNodeAction = {
+  removed: path => `Property '${pathToStr(path)}' was removed`,
+  added: (path, { valueAfter }) => `Property '${pathToStr(path)}' was added whith value: ${stringify(valueAfter)}`,
+  updated: (path, { valueBefore, valueAfter }) => `Property '${pathToStr(path)}' was updated. From ${stringify(valueBefore)} to ${stringify(valueAfter)}`,
+  nested: (path, { children }, f) => f(children, path),
+  unchanged: () => null,
+};
 
+function renderDiffToPlain(ast, pathAcc = []) {
   return ast
     .map((node) => {
       const { key, type } = node;
       const path = [...pathAcc, key];
       const render = renderNodeAction[type];
-      return render(path, node);
+      return render(path, node, renderDiffToPlain);
     })
     .filter(elem => elem !== null)
     .join('\n');
-};
+}
 
 export default renderDiffToPlain;
